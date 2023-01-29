c0b15d13e1a89fd9bbd02a2494a3224e024ecc16
rakeshrauniyar12 <rakeshrauniyar12>
2023-01-29 21:41:04 +0530

    first-commit

---
+++ Backend/AbhinandanMart/src/main/java/com/abhi/model/AdminLogin.java
@@ -0,0 +1,9 @@
+package com.abhi.model;
+
+
+public class AdminLogin {
+	
+	private String adminEmail;
+	private String adminPassword;
+
+}
---
+++ Backend/AbhinandanMart/src/main/java/com/abhi/model/UserLogin.java
@@ -0,0 +1,8 @@
+package com.abhi.model;
+
+public class UserLogin {
+	
+	private String userEmail;
+	private String password;
+
+}
