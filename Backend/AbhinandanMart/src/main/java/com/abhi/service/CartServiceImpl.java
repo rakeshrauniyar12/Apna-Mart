1ba8611380392ccc2e78620564967ac462c753eb
rakeshrauniyar12 <rakeshrauniyar12>
2023-02-18 19:30:54 +0530

    cart-service-implementation has been completed

--- Backend/AbhinandanMart/src/main/java/com/abhi/service/CartServiceImpl.java
+++ Backend/AbhinandanMart/src/main/java/com/abhi/service/CartServiceImpl.java
@@ -2,6 +2,8 @@ package com.abhi.service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -60,6 +62,7 @@ public class CartServiceImpl implements CartService{
 		            		if(list.get(r).getProductId()==productId) {
 		            			flag=true;
 		            		product=list.remove(r);
+		            		cRepo.save(user.get().getCart());;
 		            		} else {
 		            			flag=false;
 		            		}
@@ -69,18 +72,44 @@ public class CartServiceImpl implements CartService{
 		            	} else {
 		            		throw new ProductException("Product is not exist with given id");
 		            	}
+//		            	List<Product> ss=list.stream().filter(p-> p.getProductId()!=productId).collect(Collectors.toList());
+//		            	return ss;
 		             } else {
 		            	 throw new UserException("No cart is available for this user");
 		             }
-		             return product;
+		           return product;
 	}
 
 
 
 	@Override
-	public Product updateQuantityOfProduct(Integer userId,Integer productId) throws ProductException {
-		// TODO Auto-generated method stub
-		return null;
+	public Product updateQuantityOfProduct(Integer userId,Integer productId,Integer quantity) throws ProductException, UserException {
+		  Optional<User> user=uRepo.findById(userId);
+          Product product = null;
+          if(user!=null) {
+         	List<Product> list= user.get().getCart().getProduct();
+         	boolean flag=true;
+         	for(int r=0;r<list.size();r++) {
+         		if(list.get(r).getProductId()==productId) {
+         			flag=true;
+         		    list.get(r).setQuantity(list.get(r).getQuantity()+quantity);
+         		cRepo.save(user.get().getCart());;
+         		product= list.get(r);
+         		} else {
+         			flag=false;
+         		}
+         	}
+         	if(flag) {
+         		
+         	} else {
+         		throw new ProductException("Product is not exist with given id");
+         	}
+//         	List<Product> ss=list.stream().filter(p-> p.getProductId()!=productId).collect(Collectors.toList());
+//         	return ss;
+          } else {
+         	 throw new UserException("No cart is available for this user");
+          }
+        return product;     
 	}
 
 }
