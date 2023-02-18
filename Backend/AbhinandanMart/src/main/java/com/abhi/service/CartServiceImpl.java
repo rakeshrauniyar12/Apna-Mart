328f6ba2d90067979c101b2179363590cb36689a
rakeshrauniyar12 <rakeshrauniyar12>
2023-02-18 23:32:45 +0530

    implement delete functionality in cart page

--- Backend/AbhinandanMart/src/main/java/com/abhi/service/CartServiceImpl.java
+++ Backend/AbhinandanMart/src/main/java/com/abhi/service/CartServiceImpl.java
@@ -2,8 +2,6 @@ package com.abhi.service;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +12,7 @@ import com.abhi.model.Cart;
 import com.abhi.model.Product;
 import com.abhi.model.User;
 import com.abhi.repository.CartRepo;
+import com.abhi.repository.ProductRepo;
 import com.abhi.repository.UserRepo;
 @Service
 public class CartServiceImpl implements CartService{
@@ -21,6 +20,8 @@ public class CartServiceImpl implements CartService{
 	private CartRepo cRepo;
 	@Autowired
     private UserRepo uRepo;
+	@Autowired
+    private ProductRepo pRepo;
 	@Override
 	public Cart saveProduct(Integer userId, Product product) throws ProductException,UserException {
 		   Optional<User> user= uRepo.findById(userId); 
@@ -52,33 +53,44 @@ public class CartServiceImpl implements CartService{
 
 
 	@Override
-	public Product deleteProductFromCart(Integer userId,Integer productId) throws UserException,ProductException {
-		             Optional<User> user=uRepo.findById(userId);
-		            Product product = null;
-		             if(user!=null) {
-		            	List<Product> list= user.get().getCart().getProduct();
-		            	boolean flag=true;
-		            	for(int r=0;r<list.size();r++) {
-		            		if(list.get(r).getProductId()==productId) {
-		            			flag=true;
-		            		product=list.remove(r);
-		            		cRepo.save(user.get().getCart());;
-		            		} else {
-		            			flag=false;
-		            		}
-		            	}
-		            	
-		            	if(flag) {
-		            		
-		            	} else {
-		            		throw new ProductException("Product is not exist with given id");
-		            	}
-//		            	List<Product> ss=list.stream().filter(p-> p.getProductId()!=productId).collect(Collectors.toList());
-//		            	return ss;
-		             } else {
-		            	 throw new UserException("No cart is available for this user");
-		             }
-		           return product;
+	public List<Product> deleteProductFromCart(Integer userId,Integer productId) throws UserException,ProductException {
+		
+	                    Optional<User> user=uRepo.findById(userId);
+		
+		
+			Cart existingCart = user.get().getCart();
+			List<Product> listOfProducts = existingCart.getProduct();
+			Optional<Product> optProduct = pRepo.findById(productId);
+			listOfProducts.remove(optProduct.get());
+//			existingCart.getProduct().addAll(listOfProducts);
+			cRepo.save(existingCart);
+			return existingCart.getProduct();
+//		             Optional<User> user=uRepo.findById(userId);
+//		            Product product = null;
+//		             if(user!=null) {
+//		            	List<Product> list= user.get().getCart().getProduct();
+//		            	boolean flag=true;
+//		            	for(int r=0;r<list.size();r++) {
+//		            		if(list.get(r).getProductId()==productId) {
+//		            			flag=true;
+//		            		product=list.remove(r);
+//		            		cRepo.save(user.get().getCart());;
+//		            		} else {
+//		            			flag=false;
+//		            		}
+//		            	}
+//		            	
+//		            	if(flag) {
+//		            		
+//		            	} else {
+//		            		throw new ProductException("Product is not exist with given id");
+//		            	}
+////		            	List<Product> ss=list.stream().filter(p-> p.getProductId()!=productId).collect(Collectors.toList());
+////		            	return ss;
+//		             } else {
+//		            	 throw new UserException("No cart is available for this user");
+//		             }
+//		           return product;
 	}
 
 
